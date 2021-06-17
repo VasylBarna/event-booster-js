2b97a97cae5be2da67461f87bceeaf83dd064275
Denis Singh <denis.darkrgd@gmail.com>
2021-06-17 16:42:46 +0300

    Added some functional

--- src/js/loader.js
+++ src/js/loader.js
@@ -13,6 +13,20 @@ function onSiteOpen() {
   }, 2500);
 }
 
+export default function onLoaderActive() {
+  document.body.classList.toggle('loader-open');
+  refs.loader.classList.toggle('is-hidden');
+  refs.cardsList.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+
+  setTimeout(() => {
+    document.body.classList.toggle('loader-open');
+    refs.loader.classList.toggle('is-hidden');
+  }, 1500);
+}
+
 // paginationContainer: document.querySelector('#pagination-container'),
 // refs.paginationContainer.addEventListener('click', onPaginationClick);
 // allLi.addEventListener('click', onPaginationClick);
