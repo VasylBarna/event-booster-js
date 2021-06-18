aeb7f164bed7ce14084be5fd739711c76e2d3c25
Denis Singh <denis.darkrgd@gmail.com>
2021-06-18 22:27:49 +0300

    Added loader to inputs

--- src/js/input.js
+++ src/js/input.js
@@ -6,8 +6,8 @@ import SearchService from './api_service';
 import debounce from 'lodash.debounce';
 import onFetchError from '../js/pnotify.js';
 import paginationCreate from '../js/pagination.js';
+import loader from './loader.js';
 const searchService = new SearchService();
-
 
 refs.inputRef.addEventListener('input', debounce(fetchData, 1500));
 refs.selectRef.addEventListener('change', debounce(fetchCountry, 1500));
@@ -15,19 +15,25 @@ refs.selectRef.addEventListener('change', debounce(fetchCountry, 1500));
 function fetchData() {
   searchService.searchQuery = this.value.trim();
 
+  loader();
   searchService.fetchApiEvent().then(renderData);
 }
 
 function renderData(dataRender) {
   console.log(dataRender);
   if (!dataRender) {
-    
     onFetchError.onFetchNotice('No events found');
     $('#data-container').html('');
+  } else {
+    paginationCreate(dataRender);
+  }
 
-  }
-  else {paginationCreate(dataRender);
-  }
+  document.body.classList.toggle('loader-open');
+  refs.loader.classList.toggle('is-hidden');
+  refs.cardsList.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
   //var markup = eventCardsTpl(dataRender);
   //$('#data-container').html(markup);
 }
@@ -35,6 +41,13 @@ function renderData(dataRender) {
 function fetchCountry(e) {
   searchService.country = Object.entries(isoCountries)[e.target.selectedIndex - 1][0];
   searchService.fetchApiEvent().then(renderData);
+
+  document.body.classList.toggle('loader-open');
+  refs.loader.classList.toggle('is-hidden');
+  refs.cardsList.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
 }
 
 renderCountries();
