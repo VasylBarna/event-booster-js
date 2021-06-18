86a163e4ad8a5730ea48790892bf91056381fe49
VasylBarna <vasylbarna@gmail.com>
2021-06-18 16:15:45 +0300

    fix open modal

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -1,21 +1,23 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
+import { refs } from './refs';
+import card from '../templates/eventCardsTpl.hbs';
+import modalTpl from '../templates/modalTpl.hbs';
+export default (() => {
   refs.openModalBtn.addEventListener('click', onOpenModal);
   refs.closeModalBtn.addEventListener('click', onCloseModal);
 
-  function onOpenModal() {
-    document.body.classList.add('modal-open');
+  function onOpenModal(e) {
+    if (e.target.nodeName !== 'IMG' & 'P') {
+      return;
+    }
+    console.log(e.target);
+    document.body.classList.add('data-modal-open');
+
     refs.modal.classList.remove('is-hidden');
     window.addEventListener('keydown', onKeydownClose);
     refs.modal.addEventListener('click', onOverlay);
   }
   function onCloseModal() {
-    document.body.classList.remove('modal-open');
+    document.body.classList.remove('data-modal-open');
     refs.modal.classList.add('is-hidden');
     window.removeEventListener('keydown', onKeydownClose);
     refs.modal.removeEventListener('click', onOverlay);
