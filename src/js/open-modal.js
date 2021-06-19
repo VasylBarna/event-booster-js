53e14a46b0373bd6871c572a2463a03fc2463ff1
VasylBarna <vasylbarna@gmail.com>
2021-06-19 19:11:29 +0300

    center

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -1,17 +1,16 @@
+import SearchService from './api_service';
 import { refs } from './refs';
-import card from '../templates/eventCardsTpl.hbs';
 import modalTpl from '../templates/modalTpl.hbs';
 export default (() => {
   refs.openModalBtn.addEventListener('click', onOpenModal);
   refs.closeModalBtn.addEventListener('click', onCloseModal);
 
   function onOpenModal(e) {
-    if (e.target.nodeName !== 'IMG' & 'P') {
+    if ((e.target.nodeName !== 'IMG') & 'P') {
       return;
     }
     console.log(e.target);
     document.body.classList.add('data-modal-open');
-
     refs.modal.classList.remove('is-hidden');
     window.addEventListener('keydown', onKeydownClose);
     refs.modal.addEventListener('click', onOverlay);
@@ -29,3 +28,31 @@ export default (() => {
     if (e.target === refs.modal) onCloseModal();
   }
 })();
+const searchServiceId = new SearchService();
+
+// const value = searchServiceId.fetchApiById();
+// console.log(value);
+
+refs.openModalBtn.addEventListener('click', onOpenModal);
+
+function onOpenModal(e) {
+  if ((e.target.nodeName !== 'IMG') & 'P') {
+    return;
+  }
+
+  const id = e.target.dataset.id;
+  console.log('id', id);
+
+  searchServiceId
+    .fetchApiById(id)
+    // .then(id => console.log(id))
+    .then(el => modalTpl(el))
+    .then(el => (refs.mainModal.innerHTML = el));
+
+  // console.log(refs.mainModal);
+
+  // refs.mainModal.innerHTML = eventsModalTpl(value);
+
+  document.body.classList.add('data-modal-open');
+  refs.modal.classList.remove('is-hidden');
+}
